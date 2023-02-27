67468273864c04ebdfe4f2758f0ae56e1605296b
Emiliano G. Garello <emiliano.garello@gmail.com>
2023-02-27 14:02:43 -0300

    first commit

---
+++ src/main/java/com/mosconapi/mosconapi/MosconapiApplication.java
@@ -0,0 +1,13 @@
+package com.mosconapi.mosconapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosconapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosconapiApplication.class, args);
+	}
+
+}
